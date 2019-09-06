35c21c0c39a3e0243ecd892c8e6efc83dea95022
Alexis Garon-Michaud <garonmichauda@cgmatane.qc.ca>
2019-09-06 13:55:39 -0400

    Inital commit

---
+++ TricksAspNetCore/TricksAspNetCore/EnumerableExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TricksAspNetCore
+{
+    public static class EnumerableExtensions
+    {
+        // This extension helps to avoid null checking against Enumerable
+        public static IEnumerable<T> OrEmptyIfNull<T>(this IEnumerable<T> source) =>
+            source ?? Enumerable.Empty<T>();
+    }
+}
---
+++ TricksAspNetCore/TricksAspNetCore/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TricksAspNetCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // TIP 1 - Simple demo to avoid null checking on an Enumerable
+
+            List<string> names = GetNullList();
+
+            foreach (var name in names.OrEmptyIfNull())
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+
+        // Method that could return an null list
+        private static List<string> GetNullList() => null;
+    }
+}
